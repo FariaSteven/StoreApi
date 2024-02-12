d6d8c3e608415ab8bb7edcce59b72c9567c4dd5b
Steven Faria <stevenfaria.gusmao@gmail.com>
2024-02-12 18:25:12 -0300

    first commit

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+
+namespace Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+
+        services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(assembly));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        return services;
+    }
+}
+
